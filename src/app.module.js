3c122b8dc544fd8860462b3166404032dfaba30a
ditaisyiyah <ditalara19@gmail.com>
2022-01-20 09:13:56 +0700

    move app module inside app folder

--- src/app.module.js
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CatModule } from './cat/cat.module';
-import { KittenModule } from './kitten/kitten.module';
-
-@Module({
-  imports: [CatModule, KittenModule],
-})
-export class AppModule {}
